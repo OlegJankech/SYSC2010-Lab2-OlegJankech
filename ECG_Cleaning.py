824acc6de290fdbc856a8c44707c25e6eae80350
Oleg Jankech <olegjankech@gmail.com>
2026-02-04 13:54:37 -0500

    Completed

--- ECG_Cleaning.py
+++ ECG_Cleaning.py
@@ -23,17 +23,24 @@ lead_original = data['lead_I']
 mean = np.mean(lead_original)
 
 # subtract mean from signal
-lead_processed = lead_original - mean
+lead_processed = lead_original
 
-signal = np.convolve(lead_processed,np.ones(100)/100,mode='same')
+signal = np.convolve(lead_processed,np.ones(20)/20,mode='same')
 
+# save processed data to new csv without date
+processed_data = pd.DataFrame({'t': data['t'], 'lead_I': signal})
+processed_data.to_csv('processed_ecg.csv', index=False)
+
+time = time[250:750]
+lead_original = lead_original[250:750]
+signal = signal[250:750]
 
 # temp vs time
 plt.figure()
-plt.plot(time, signal)
-plt.plot(time, lead_original)
+plt.plot(data['t'][250:750], lead_original)
+plt.plot(data['t'][250:750], signal)
 plt.xlabel(ECG_GUI.gui.x)
 plt.ylabel(ECG_GUI.gui.y)
-plt.title("Lead I vs Time")
-#plt.xticks(rotation=45)
+plt.title("Processed and Unprocessed Lead I")
+plt.xticks(rotation=45)
 plt.show()
