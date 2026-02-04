6e4214edcd89f2b8a2253078d8c536db94962d29
Oleg Jankech <olegjankech@gmail.com>
2026-02-04 11:41:59 -0500

    Update ECG_Cleaning.py

--- ECG_Cleaning.py
+++ ECG_Cleaning.py
@@ -1,3 +1,18 @@
 import ECG_GUI
+import pandas as pd
+import matplotlib as plt
 
-ECG_GUI.gui.csv
+data = pd.read_csv(ECG_GUI.gui.csv)
+
+
+time = pd.to_datetime(data['t']) # unreadable otherwise
+temp = data['temperature_C']
+humidity = data['humidity_pct']
+
+# temp vs time
+plt.figure()
+plt.plot(time, temp)
+plt.xlabel("Time (s)")
+plt.ylabel("Temperature (°C)")
+plt.title("Temperature vs Time")
+plt.xticks(rotation=45)
